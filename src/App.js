230a593f6ed94b826f7abe88843a153ffeca8c1c
John Meguerian <johnmeguerian@gmail.com>
2017-08-29 09:14:34 -0400

    the app works!
    on submit, an h2 tag is rendered with the computed name and the input value is reset.
    next step is styling it.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ class App extends Component {
   }
 
   alphabetize = input => {
-    const alpha = str => str.split("").sort().join("")
+    const alpha = str =>
+      str
+        .split("")
+        .sort()
+        .join("")
     let words = input.split(" ")
     let output = words
       .map(word => {
@@ -24,18 +28,27 @@ class App extends Component {
     return output
   }
 
+  handleSubmit = e => {
+    e.preventDefault()
+    const name = this.alphabetize(this.state.input)
+    this.setState({ input: "" })
+    this.setState({ output: name })
+  }
+
   render() {
     return (
       <div>
-        <input
-          type="text"
-          name="input"
-          placeholder="Type your name"
-          onChange={e => this.handleChange(e.target.value)}
-        />
-        <h2>
-          {this.output}
-        </h2>
+        <h2>{this.state.output}</h2>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            name="input"
+            value={this.state.input}
+            placeholder="Type your name"
+            onChange={e => this.handleChange(e.target.value)}
+          />
+          <button type="submit">Generate Name</button>
+        </form>
       </div>
     )
   }
